ebdea8770b233d6de501fabb63af16334fe69da1
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2024-02-16 12:47:32 -0300

    modificacion segundo numero

---
+++ practicas de clase/clase 1 variables y sintaxis/main.js
@@ -0,0 +1,13 @@
+//aqui dentro escribo mi programacion de java script//
+
+
+
+let numero_1 = Number(prompt("ingrese el primer numero") )
+
+let numero_2 = Number(prompt("ingrese el numerod deseado"))
+
+
+console.log("la suma entre " + numero_1 + " y " + numero_2 + " es " + (numero_1 + numero_2))
+
+
+
